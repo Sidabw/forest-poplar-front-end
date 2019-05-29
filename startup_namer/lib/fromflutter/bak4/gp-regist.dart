87633c7f58d2b6ff0a1d64563bdd5f4e09966e08
feiyi <feiyi@zenki.ai>
2019-05-29 14:23:55 +0800

    flutter:dio发送请求，Image.network获取图片，图片点击事件，Row水平排列等

--- startup_namer/lib/fromflutter/bak4/gp-regist.dart
+++ startup_namer/lib/fromflutter/bak4/gp-regist.dart
@@ -1,6 +1,8 @@
 //手机验证码注册页面
 
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'dart:math';
 
 class RegistPage extends StatefulWidget {
   @override
@@ -10,6 +12,11 @@ class RegistPage extends StatefulWidget {
 class _RegistPageState extends State<RegistPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  final List<Image> aa = List<Image>();
+  Image verifyimg =  Image.asset(
+                            'assets/sidabw.png',
+                             height: 46.0,
+                             width: 46.0,);
 
   @override
   Widget build(BuildContext context) {
@@ -38,9 +45,40 @@ class _RegistPageState extends State<RegistPage> {
                     filled: true,//filled 指该文本框的背景将被一个浅色填充，以助于用户分辨出点击或触摸区域
                     labelText: '手机号📱',
                   ),
-                    keyboardType:TextInputType.number,  //数字键盘
+                  keyboardType:TextInputType.number,  //数字键盘
                 ),
                 SizedBox(height: 12.0,),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      filled: true,//filled 指该文本框的背景将被一个浅色填充，以助于用户分辨出点击或触摸区域
+                      labelText: '图形验证码',
+                    ),
+                    onTap: (){
+                      var randomValue = Random().nextInt(100).toString();
+                      verifyimg = Image.network("http://10.0.2.2:8017/gp/verify/getImg/" + randomValue,width: 135, height: 50,);
+                      setState(() {});
+                    },
+                  ),
+                ),
+                SizedBox(width: 18.0,),
+                Expanded(
+                  child: IconButton(
+                      icon: verifyimg,
+                      iconSize: 48,
+                      onPressed: () {
+                        //All network images are cached regardless of HTTP headers。所以就只能让你url每次都不一样喽...
+                        var randomValue = Random().nextInt(100).toString();
+                        verifyimg = Image.network("http://10.0.2.2:8017/gp/verify/getImg/" + randomValue,width: 135, height: 50,);
+                        setState(() {});
+                      }
+                  ),
+                )
               ],
             ),
             ButtonBar(
@@ -49,14 +87,23 @@ class _RegistPageState extends State<RegistPage> {
                 RaisedButton(
                   child: Text('获取手机验证码'),
                   onPressed: (){
-                    Navigator.pop(context);
+
                   },
                 )
               ],
-            )
+            ),
           ],
         ),
       ),
     );
+  }
+
+  void getHttp() async {
+    try {
+      Response response = await Dio().get("http://10.0.2.2:8017/");
+      print(response);
+    } catch (e) {
+      print(e);
+    }
   }
 }
